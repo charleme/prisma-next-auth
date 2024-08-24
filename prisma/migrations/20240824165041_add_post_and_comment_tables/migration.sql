4de6d95ea41c513ff49e67387c28bf64a0fb1dd2
ceb <ce.bray@thecodingmachine.com>
2024-08-24 19:35:51 +0200

    feat: implement post and comment tables

---
+++ prisma/migrations/20240824165041_add_post_and_comment_tables/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT,
+    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "content" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+    CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
---
+++ prisma/seed/post.ts
@@ -0,0 +1,24 @@
+import { fakerFR as faker } from "@faker-js/faker";
+import { type DbClient } from "~/server/db";
+
+export const seedPost = async (prisma: DbClient) => {
+  const now = new Date();
+
+  const users = await prisma.user.findMany({
+    take: 30,
+  });
+
+  const posts = [];
+  for (let i = 0; i < 90; i++) {
+    const author = faker.helpers.arrayElement(users);
+    posts.push({
+      title: faker.lorem.sentence(),
+      content: faker.lorem.paragraphs({ min: 1, max: 5 }),
+      authorId: author.id,
+      published: faker.datatype.boolean(),
+      createdAt: faker.date.between({ from: author.createdAt, to: now }),
+    });
+  }
+
+  await prisma.post.createMany({ data: posts });
+};

fff9c1bcb601d9a88dfc932aefd1cbd4f568c7a9
ceb <ce.bray@thecodingmachine.com>
2024-09-14 09:52:18 +0200

    feat: implement post list actions

--- src/app/(logged)/post/post-columns.tsx
+++ src/app/(logged)/post/post-columns.tsx
@@ -2,6 +2,16 @@ import { type ColumnDefWithViewSelectorMeta } from "~/types/data-table";
 import { type PostSearchItem } from "~/types/query/post/search";
 import { DataTableColumnHeader } from "~/components/molecule/data-table/data-column-header";
 import { cn } from "~/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { Button } from "~/components/ui/button";
+import { MoreHorizontal } from "lucide-react";
 
 export const postColumns: ColumnDefWithViewSelectorMeta<PostSearchItem>[] = [
   {
@@ -42,6 +52,31 @@ export const postColumns: ColumnDefWithViewSelectorMeta<PostSearchItem>[] = [
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Comment number" />
     ),
+  },
+  {
+    id: "actions",
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-40">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View post</DropdownMenuItem>
+            <DropdownMenuItem>View author</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-destructive">
+              Delete post
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
   {
     id: "ownPost", // column only use for filtering

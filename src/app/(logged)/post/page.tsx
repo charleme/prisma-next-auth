23b3a5509731cbc61dd72e49236fda24b037376b
ceb <ce.bray@thecodingmachine.com>
2024-09-14 10:11:41 +0200

    feat: add create post button

--- src/app/(logged)/post/page.tsx
+++ src/app/(logged)/post/page.tsx
@@ -6,6 +6,8 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 import { PostDataTable } from "~/app/(logged)/post/post-data-table";
+import { Button } from "~/components/ui/button";
+import { Plus } from "lucide-react";
 
 export default function PostListPage() {
   return (
@@ -16,7 +18,12 @@ export default function PostListPage() {
           <CardDescription>View the posts of the app</CardDescription>
         </CardHeader>
         <CardContent>
-          <PostDataTable />
+          <PostDataTable>
+            <Button size="sm" className="ml-auto h-8">
+              <Plus className="mr-2 h-4 w-4 " />
+              Create Post
+            </Button>
+          </PostDataTable>
         </CardContent>
       </Card>
     </div>

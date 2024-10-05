1afe591cf0892fcec11a308cca13d7d2abb340c1
ceb <ce.bray@thecodingmachine.com>
2024-10-05 16:46:09 +0200

    feat: rename router files

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,8 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
-import { userRouter } from "~/server/api/routers/user";
+import { userRouter } from "~/server/api/routers/user-router";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
-import { roleRouter } from "~/server/api/routers/role";
-import { postRouter } from "~/server/api/routers/post";
+import { roleRouter } from "~/server/api/routers/role-router";
+import { postRouter } from "~/server/api/routers/post-router";
 
 /**
  * This is the primary router for your server.
